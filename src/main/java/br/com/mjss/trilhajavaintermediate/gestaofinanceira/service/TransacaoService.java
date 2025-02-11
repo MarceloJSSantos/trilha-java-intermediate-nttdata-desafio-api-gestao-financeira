fbf73e6954f2896c8727a6a96d2cc907e1cdf332
MarceloJSSantos <marcelojssantos2012@gmail.com>
2025-02-11 16:44:44 -0300

    Transacao * Atualização do service - refatoração   * validações

--- src/main/java/br/com/mjss/trilhajavaintermediate/gestaofinanceira/service/TransacaoService.java
+++ src/main/java/br/com/mjss/trilhajavaintermediate/gestaofinanceira/service/TransacaoService.java
@@ -1,7 +1,10 @@
 package br.com.mjss.trilhajavaintermediate.gestaofinanceira.service;
 
 import br.com.mjss.trilhajavaintermediate.gestaofinanceira.dto.transacao.*;
-import br.com.mjss.trilhajavaintermediate.gestaofinanceira.model.transacao.*;
+import br.com.mjss.trilhajavaintermediate.gestaofinanceira.model.transacao.Categoria;
+import br.com.mjss.trilhajavaintermediate.gestaofinanceira.model.transacao.Metodo;
+import br.com.mjss.trilhajavaintermediate.gestaofinanceira.model.transacao.TipoTransacao;
+import br.com.mjss.trilhajavaintermediate.gestaofinanceira.model.transacao.Transacao;
 import br.com.mjss.trilhajavaintermediate.gestaofinanceira.model.usuario.Usuario;
 import br.com.mjss.trilhajavaintermediate.gestaofinanceira.repository.TransacaoComSaldoViewRepository;
 import br.com.mjss.trilhajavaintermediate.gestaofinanceira.repository.TransacaoRepository;
@@ -20,117 +23,117 @@ import java.time.format.DateTimeParseException;
 @Service
 public class TransacaoService {
 
+    DateTimeFormatter formatoDataDDMMAAAA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     @Autowired
     private TransacaoRepository repository;
-
     @Autowired
     private TransacaoComSaldoViewRepository repositoryTransacaoComSaldo;
-
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    DateTimeFormatter formatoDataDDMMAAAA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-    public Transacao cadastrar(TransacaoCadastroDTO dto){
+    public Transacao cadastrar(TransacaoCadastroDTO dto) {
         var usuario = usuarioRepository.getReferenceById(dto.idUsuario());
         var transacao = new Transacao(usuario, dto);
 
         validaSeUsuarioExiste(dto.idUsuario());
-        validaSeMetodoAdequadoComTipo(dto.tipo(), dto.metodo());
-        validaSeCategoriaAdequadaComTipo(dto.tipo(), dto.categoria());
-        validaSeValorAdequadoComTipo(dto.valor(), dto.tipo());
+
+        validaSeCategoriaAdequadaComTipoParaCadastro(dto.tipo(), dto.categoria());
+        validaSeMetodoAdequadoComTipoParaCadastro(dto.tipo(), dto.metodo());
+        validaSeValorAdequadoComTipoParaCadastro(dto.tipo(), dto.valor());
 
         return repository.save(transacao);
     }
 
-    public TransacaoDadosAposCadastroOuAtualizacaoDTO consultarTransacao(Long id){
-        try{
-            var transacao = repository.getReferenceById(id);
-            var dto = new TransacaoDadosAposCadastroOuAtualizacaoDTO(transacao);
-            return dto;
-        } catch (EntityNotFoundException e){
-            throw new EntityNotFoundException("A transação com ID '%s' não foi encontrado!".formatted(id));
-        }
+    public TransacaoDadosAposCadastroOuAtualizacaoDTO consultarTransacao(Long id) {
+        validaSeTransacaoExiste(id);
+
+        var transacao = repository.getReferenceById(id);
+        var dto = new TransacaoDadosAposCadastroOuAtualizacaoDTO(transacao);
+        return dto;
     }
 
-    public void excluirTransacao(Long id){
-        if (repository.existsById(id))
-            repository.deleteById(id);
-        else {
-            throw new EntityNotFoundException("A transação com ID '%s' não foi encontrado!".formatted(id));
-        }
+    public void excluirTransacao(Long id) {
+        validaSeTransacaoExiste(id);
+
+        repository.deleteById(id);
     }
 
     public TransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO listarTransacoesComSaldoDeUsuarioPorPeriodo(Pageable paginacao, Long usuarioId, String dataInicial, String dataFinal) {
-        if (usuarioRepository.existsById(usuarioId)){
-            var usuario = usuarioRepository.getReferenceById(usuarioId);
-            return getTransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO(paginacao, usuarioId, dataInicial, dataFinal, usuario);
-        } else{
-            throw new EntityNotFoundException("O usuário com ID '%s' não foi encontrado!".formatted(usuarioId));
-        }
+        validaSeUsuarioExiste(usuarioId);
+
+        var usuario = usuarioRepository.getReferenceById(usuarioId);
+        return getTransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO(paginacao, usuarioId, dataInicial, dataFinal, usuario);
     }
 
     public Transacao atualizarTransacao(TransacaoAtualizacaoDTO dto) {
-        final String MENSAGEM_EXCEPTION = "Para atualizar 'tipo' ou 'categoria' ou 'metodo' ou 'valor' todos tem que ser enviados!";
-        try {
-            var transacao = repository.getReferenceById(dto.id());
+        validaSeTransacaoExiste(dto.id());
+        var transacao = repository.getReferenceById(dto.id());
 
-            var seTipoOuCategoriaENull = (dto.tipo() == null || dto.categoria() == null);
-            var seTipoECategoriaNaoENull = !(dto.tipo() == null && dto.categoria() == null);
-            if(seTipoECategoriaNaoENull){
-                if(seTipoOuCategoriaENull){
-                    throw new IllegalArgumentException(MENSAGEM_EXCEPTION);
-                }
-            }
+        validaSeTipoCategoriaMetodoValorSaoEnviados(dto);
 
-            var seTipoOuMetodoENull = (dto.tipo() == null || dto.metodo() == null);
-            var seTipoEMetodoNaoENull = !(dto.tipo() == null && dto.metodo() == null);
-            if (seTipoEMetodoNaoENull){
-                if(seTipoOuMetodoENull){
-                    throw new IllegalArgumentException(MENSAGEM_EXCEPTION);
-                }
-            }
+        validaSeCategoriaAdequadaComTipoParaAtualizacao(dto.tipo(), dto.categoria());
+        validaSeMetodoAdequadaComTipoParaAtualizacao(dto.tipo(), dto.metodo());
+        validaSeValorAdequadaComTipoParaAtualizacao(dto.tipo(), dto.valor());
 
-            var seTipoOuValorENull = (dto.tipo() == null || dto.valor() == null);
-            var seTipoEValorNaoENull = !(dto.tipo() == null && dto.valor() == null);
-            if(seTipoEValorNaoENull){
-                if(seTipoOuValorENull){
-                    throw new IllegalArgumentException(MENSAGEM_EXCEPTION);
-                }
-            }
+        transacao.atualizarTransacao(dto);
 
-            var seTipoECategoriaNaoSaoNull = (dto.tipo() != null && dto.categoria() != null);
-            if (seTipoECategoriaNaoSaoNull){
-                validaSeCategoriaAdequadaComTipo(dto.tipo(), dto.categoria());
-            }
+        return transacao;
+    }
 
-            var seTipoEMetodoNaoSaoNull = (dto.tipo() != null && dto.metodo() != null);
-            if (seTipoEMetodoNaoSaoNull){
-                validaSeMetodoAdequadoComTipo(dto.tipo(), dto.metodo());
-            }
-
-            var seTipoEValorNaoSaoNull = (dto.tipo() != null && dto.valor() != null);
-            if(seTipoEValorNaoSaoNull){
-                validaSeValorAdequadoComTipo(dto.valor(), dto.tipo());
-            }
-
-            transacao.atualizarTransacao(dto);
-
-            return transacao;
-        } catch (EntityNotFoundException e){
-            throw new EntityNotFoundException("A transação com ID '%s' não foi encontrado!".formatted(dto.id()));
+    private void validaSeValorAdequadaComTipoParaAtualizacao(TipoTransacao tipo, BigDecimal valor) {
+        var seTipoEValorNaoSaoNull = (tipo != null && valor != null);
+        if (seTipoEValorNaoSaoNull) {
+            validaSeValorAdequadoComTipoParaCadastro(tipo, valor);
         }
     }
 
-    private void validaSeUsuarioExiste(Long idUsuario) {
-        var seExisteUsuario = (usuarioRepository.existsById(idUsuario));
-
-        if (!seExisteUsuario){
-            throw new IllegalArgumentException("O ID %d não corresponde a nenhum usuário.".formatted(idUsuario));
+    private void validaSeMetodoAdequadaComTipoParaAtualizacao(TipoTransacao tipo, Metodo metodo) {
+        var seTipoEMetodoNaoSaoNull = (tipo != null && metodo != null);
+        if (seTipoEMetodoNaoSaoNull) {
+            validaSeMetodoAdequadoComTipoParaCadastro(tipo, metodo);
         }
     }
 
-    private void validaSeCategoriaAdequadaComTipo(TipoTransacao tipo, Categoria categoria){
+    private void validaSeCategoriaAdequadaComTipoParaAtualizacao(TipoTransacao tipo, Categoria categoria) {
+        var seTipoECategoriaNaoSaoNull = (tipo != null && categoria != null);
+        if (seTipoECategoriaNaoSaoNull) {
+            validaSeCategoriaAdequadaComTipoParaCadastro(tipo, categoria);
+        }
+    }
+
+    private static void validaSeTipoCategoriaMetodoValorSaoEnviados(TransacaoAtualizacaoDTO dto) {
+        var seTipoOuCategoriaENull = (dto.tipo() == null || dto.categoria() == null || dto.metodo() == null || dto.valor() == null);
+        var seTipoECategoriaNaoENull = !(dto.tipo() == null && dto.categoria() == null && dto.metodo() == null && dto.valor() == null);
+        if (seTipoECategoriaNaoENull) {
+            if (seTipoOuCategoriaENull) {
+                throw new IllegalArgumentException("Para atualizar 'tipo' ou 'categoria' ou 'metodo' ou 'valor' todos tem que ser enviados!");
+            }
+        }
+    }
+
+    private void validaSeDataInicialMenorIgualFinal(LocalDate dataInicial, LocalDate dataFinal) {
+        if (dataInicial.isAfter(dataFinal)) {
+            throw new IllegalArgumentException("A data inicial '%s' deve ser menor ou igual a data final '%s'.".formatted(dataInicial.format(formatoDataDDMMAAAA), dataFinal.format(formatoDataDDMMAAAA)));
+        }
+    }
+
+    private void validaSeUsuarioExiste(Long usuarioId) {
+        var seExisteUsuario = (usuarioRepository.existsById(usuarioId));
+
+        if (!seExisteUsuario) {
+            throw new IllegalArgumentException("O usuário com ID '%s' não foi encontrado!".formatted(usuarioId));
+        }
+    }
+
+    private void validaSeTransacaoExiste(Long id) {
+        var seExisteTransacao = (repository.existsById(id));
+
+        if (!seExisteTransacao) {
+            throw new EntityNotFoundException("A transação com ID '%s' não foi encontrada!".formatted(id));
+        }
+    }
+
+    private void validaSeCategoriaAdequadaComTipoParaCadastro(TipoTransacao tipo, Categoria categoria) {
         var seTipoTransacaoEDespesaEseTipoDaCategoriaNaoEDespesa = (tipo == TipoTransacao.DESPESA && !categoria.getTipo().equals(TipoTransacao.DESPESA));
         var seTipoTransacaoEReceitaEseTipoDaCategoriaNaoEReceita = (tipo == TipoTransacao.RECEITA && !categoria.getTipo().equals(TipoTransacao.RECEITA));
         var mensagem = "Categoria %s é inválida para tipo %s.".formatted(categoria, tipo);
@@ -142,7 +145,7 @@ public class TransacaoService {
         }
     }
 
-    private void validaSeMetodoAdequadoComTipo(TipoTransacao tipo, Metodo metodo){
+    private void validaSeMetodoAdequadoComTipoParaCadastro(TipoTransacao tipo, Metodo metodo) {
         var seTipoTransacaoEDespesaEseTipoDoMetodoNaoEDespesa = (tipo == TipoTransacao.DESPESA && !metodo.getTipo().equals(TipoTransacao.DESPESA));
         var seTipoTransacaoEReceitaEseTipoDoMetodoNaoEReceita = (tipo == TipoTransacao.RECEITA && !metodo.getTipo().equals(TipoTransacao.RECEITA));
         var mensagem = "Metodo %s é inválido para tipo %s.".formatted(metodo, tipo);
@@ -154,47 +157,50 @@ public class TransacaoService {
         }
     }
 
-    private void validaSeValorAdequadoComTipo(BigDecimal valorAtual, TipoTransacao tipo){
+    private void validaSeValorAdequadoComTipoParaCadastro(TipoTransacao tipo, BigDecimal valorAtual) {
         var seTipoTransacaoReceita = tipo.equals(TipoTransacao.RECEITA);
         var seValorAtualPositivo = valorAtual.signum() == 1;
         var seValorAtualZero = valorAtual.signum() == 0;
         var seTipoTransacaoReceitaEValorAtualPositivo = (seTipoTransacaoReceita == seValorAtualPositivo);
         var mensagem = "O 'valor' %.2f não é apropriado para o 'tipo' %s.".formatted(valorAtual, tipo);
 
-        if (!seTipoTransacaoReceitaEValorAtualPositivo || seValorAtualZero){
+        if (!seTipoTransacaoReceitaEValorAtualPositivo || seValorAtualZero) {
             throw new IllegalArgumentException(mensagem);
         }
     }
 
-    private LocalDateTime converteParaLocalDateTime(String dateString, boolean finalDia) {
-        if (dateString == null){
-            if (finalDia){
-                dateString = LocalDate.now().format(formatoDataDDMMAAAA).toString();
-            } else{
-                dateString = "01/01/1900";
-            }
-        }
+    private LocalDateTime converteParaLocalDateTime(String data, boolean finalDia) {
+        data = getData(data, finalDia);
 
-        var horario = (finalDia)?" 23:59:59":" 00:00:00";
+        var horario = (finalDia) ? " 23:59:59" : " 00:00:00";
 
         try {
-            LocalDateTime localDateTime = LocalDateTime.parse(dateString + horario, DateTimeFormatter
-                    .ofPattern("dd/MM/yyyy HH:mm:ss"));
+            LocalDateTime localDateTime = LocalDateTime.parse(data + horario, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
             return localDateTime;
         } catch (DateTimeParseException e) {
-            throw new IllegalArgumentException("A data informada '%s' não está no formato válido 'dd/MM/AAAA'."
-                    .formatted(dateString));
+            throw new IllegalArgumentException("A data informada '%s' não está no formato válido 'dd/MM/AAAA'.".formatted(data));
         }
     }
 
-    private BigDecimal getSaldo(Usuario usuario, LocalDateTime dataIncial, LocalDateTime dataFinal){
-        var retornoTratado = BigDecimal.ZERO;
+    private String getData(String dataTratada, boolean finalDia) {
+        if (dataTratada == null) {
+            if (finalDia) {
+                dataTratada = LocalDate.now().format(formatoDataDDMMAAAA);
+            } else {
+                dataTratada = "01/01/1900";
+            }
+        }
+        return dataTratada;
+    }
+
+    private BigDecimal getSaldo(Usuario usuario, LocalDateTime dataIncial, LocalDateTime dataFinal) {
+        var saldoTratado = BigDecimal.ZERO;
         var saldoAnterior = repositoryTransacaoComSaldo.findTopByUsuarioAndDataHoraTransacaoBetweenOrderByDataHoraTransacaoDescIdDesc(usuario, dataIncial, dataFinal);
-        if (saldoAnterior.isPresent()){
-            retornoTratado = saldoAnterior.get().getSaldo();
+        if (saldoAnterior.isPresent()) {
+            saldoTratado = saldoAnterior.get().getSaldo();
         }
 
-        return retornoTratado;
+        return saldoTratado;
     }
 
     private TransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO getTransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO(Pageable paginacao, Long usuarioId, String dataInicial, String dataFinal, Usuario usuario) {
@@ -202,25 +208,14 @@ public class TransacaoService {
         var dataAtualFinal = converteParaLocalDateTime(dataFinal, true);
 
         validaSeDataInicialMenorIgualFinal(dataAtualInicial.toLocalDate(), dataAtualFinal.toLocalDate());
-        var listaPaginada = repositoryTransacaoComSaldo.findAllByUsuarioAndDataHoraTransacaoBetween(paginacao, usuario, dataAtualInicial, dataAtualFinal)
-                .map(TransacaoComSaldoDadosListagemDTO::new);
+        var listaPaginada = repositoryTransacaoComSaldo.findAllByUsuarioAndDataHoraTransacaoBetween(paginacao, usuario, dataAtualInicial, dataAtualFinal).map(TransacaoComSaldoDadosListagemDTO::new);
         var saldoAtual = getSaldo(usuario, dataAtualInicial, dataAtualFinal);
 
         var dataAnteriorInicial = converteParaLocalDateTime(null, false);
         var dataAnteriorFinal = dataAtualInicial.minusSeconds(1L);
         var saldoAnterior = getSaldo(usuario, dataAnteriorInicial, dataAnteriorFinal);
 
-        var resposta = new TransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO(
-                dataAtualInicial.toLocalDate().format(formatoDataDDMMAAAA).toString(),
-                dataAtualFinal.toLocalDate().format(formatoDataDDMMAAAA).toString(),
-                saldoAnterior, saldoAtual, usuarioId, listaPaginada );
+        var resposta = new TransacaoDadosAposConsultaComSaldoDeUsuarioPorPeriodoDTO(dataAtualInicial.toLocalDate().format(formatoDataDDMMAAAA), dataAtualFinal.toLocalDate().format(formatoDataDDMMAAAA), saldoAnterior, saldoAtual, usuarioId, listaPaginada);
         return resposta;
-    }
-
-    private void validaSeDataInicialMenorIgualFinal(LocalDate dataInicial, LocalDate dataFinal) {
-        if (dataInicial.isAfter(dataFinal)){
-            throw new IllegalArgumentException("A data inicial '%s' deve ser menor ou igual a data final '%s'."
-                    .formatted(dataInicial.format(formatoDataDDMMAAAA), dataFinal.format(formatoDataDDMMAAAA)));
-        }
     }
 }
